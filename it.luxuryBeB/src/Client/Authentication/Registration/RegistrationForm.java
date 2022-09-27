82e87ecd8fbdbbc29d5b3c5f8e3af93988a50790
Filomena <filomena.derosa001@studenti.uniparthenope.it>
2022-09-27 10:39:23 +0200

    Aggiunto Swing

--- it.luxuryBeB/src/Client/Authentication/Registration/RegistrationForm.java
+++ it.luxuryBeB/src/Client/Authentication/Registration/RegistrationForm.java
@@ -17,6 +17,32 @@ public class RegistrationForm implements Form {
 
     @Override
     public void createForm() {
+        email = new JTextField();
+        password = new JPasswordField();
+        conferma = new JButton("Conferma");
+        nome = new JTextField();
+        cognome = new JTextField();
+        codicefiscale = new JTextField();
+        em = new JLabel("E-mail:");
+        ps = new JLabel("Password:");
+        cnf = new JLabel("Conferma");
+        nom = new JLabel("Nome");
+        cgn = new JLabel("Cognome");
+        cf = new JLabel("CodiceFiscale");
+
+
+        email.setBounds(50, 100, 200, 30);
+        password.setBounds(50, 160, 200, 30);
+        conferma.setBounds(50, 220, 200, 30);
+        nome.setBounds(50,80,210,30);
+        cognome.setBounds(50, 80,210,30);
+        codicefiscale.setBounds(50,80,210,30);
+        em.setBounds(50, 70, 240, 30);
+        ps.setBounds(50, 130, 240, 30);
+        cnf.setBounds(50, 400, 230, 30);
+        nom.setBounds(50,80,210,30);
+        cgn.setBounds(50,80,210,30);
+        cf.setBounds(50,80,210,30);
 
     }
 
