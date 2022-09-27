7cb040aef367a47c03ae550d4def07d84aca556b
Filomena <filomena.derosa001@studenti.uniparthenope.it>
2022-09-27 16:07:09 +0200

    Aggiunto Swing

--- it.luxuryBeB/src/Owner/FactoryMaker.java
+++ it.luxuryBeB/src/Owner/FactoryMaker.java
@@ -2,6 +2,7 @@ package Owner;
 
 
 import Owner.Interface.AbstractFactory;
+import Owner.init.InitFactory;
 
 public class FactoryMaker {
     private static FactoryMaker Instance=null;
@@ -15,7 +16,7 @@ public class FactoryMaker {
 
     public AbstractFactory getFactory(String factory){
         return switch (factory){
-
+            case "init" -> new InitFactory();
             default -> null;
         };
     }
---
+++ it.luxuryBeB/src/Owner/init/InitFactory.java
@@ -0,0 +1,32 @@
+package Owner.init;
+
+import Owner.Interface.AbstractFactory;
+import Owner.Interface.Template.Button;
+import Owner.Interface.Template.Form;
+import Owner.Interface.Template.Text;
+
+public class InitFactory implements AbstractFactory {
+
+    @Override
+    public Form createForm(String type) {
+        return switch (type){
+             case "viewbookedrooms" -> new BookedForm();
+             case "insertroom" -> new InsertRoomForm();
+             default -> null;
+        };
+    }
+
+    @Override
+    public Button createButton(String type) {
+        return switch (type){
+            case "viewbookedrooms" -> new BookedButton();
+            case "insertroom" -> new InsertRoomButton();
+            default -> null;
+        };
+    }
+
+    @Override
+    public Text createText(String type) {
+        return null;
+    }
+}
