8726e82947235406e14a572d19aa9e67cdb7a297
Filomena <filomena.derosa001@studenti.uniparthenope.it>
2022-10-03 16:57:34 +0200

    swing swing

--- it.luxuryBeB/src/Client/Authentication/Login/LoginForm.java
+++ it.luxuryBeB/src/Client/Authentication/Login/LoginForm.java
@@ -6,12 +6,17 @@ import Client.Interface.Template.Form;
 import Client.Private.PrivateConcreteHandler;
 
 import javax.swing.*;
+import java.awt.*;
 
 public class LoginForm implements Form {
     private static JTextField email=null;
     private static JPasswordField password=null;
     private JButton login=null;
     private JLabel em, ps, lg= null;
+    private JLayeredPane layer = null;
+    private JLabel rettangolo;
+
+
 
     @Override
     public void createForm() {
@@ -21,6 +26,10 @@ public class LoginForm implements Form {
         em = new JLabel("E-mail:");
         ps = new JLabel("Password:");
         lg = new JLabel("Area Login");
+        layer = new JLayeredPane();
+        rettangolo = new JLabel();
+        layer.setPreferredSize(new Dimension(200,200));
+        layer.setBounds(0,0,300,300);
 
         email.setBounds(50, 100, 200, 30);
         password.setBounds(50, 160, 200, 30);
@@ -28,13 +37,22 @@ public class LoginForm implements Form {
         em.setBounds(50, 70, 240, 30);
         ps.setBounds(50, 130, 240, 30);
         lg.setBounds(50, 40, 230, 30);
+        rettangolo.setBackground(Color.CYAN);
+        rettangolo.setBounds(0,0,500,500);
 
         login.setActionCommand("Authentication-Form-Login");
         login.addActionListener(AuthenticationConcreteHandler.getInstance());
+
+        layer.add(rettangolo, Integer.valueOf(1));
+        layer.add(email,Integer.valueOf(2));
+        layer.add(password,Integer.valueOf(2));
+        layer.add(login, Integer.valueOf(2));
+        layer.add(em, Integer.valueOf(2));
+        layer.add(ps,Integer.valueOf(2));
+        layer.add(lg,Integer.valueOf(2));
     }
 
     public static void comunicate(){
-        System.out.println("PIPPO");
         String mail = email.getText();
         String passw = new String(password.getPassword());
         if (mail.equals("")){
@@ -48,13 +66,12 @@ public class LoginForm implements Form {
         server.write(serverrequest.toString());
     }
 
+
+
     @Override
     public void insertForm(JFrame finestra) {
-        finestra.add(email);
-        finestra.add(password);
-        finestra.add(login);
-        finestra.add(em);
-        finestra.add(ps);
-        finestra.add(lg);
+       finestra.add(layer);
+       finestra.setSize(new Dimension(350,400));
     }
 }
+
